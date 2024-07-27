ca2e7e67ff824f7156e42dfe9dc1b1d4cc5841b5
parineeta-17 <parineeta1707@gmail.com>
2024-07-27 22:14:25 +0530

    Add files via upload

---
+++ encoder_huffman_coding.py
@@ -0,0 +1,119 @@
+print("Huffman Compression Program")
+print("=================================================================")
+h = int(input("Enter 1 if you want to enter in command window, 2 if you are using some file:"))
+if h == 1:
+    my_string = input("Enter the string you want to compress:")
+elif h == 2:
+    file = input("Enter the filename:")
+    with open(file, 'r') as f:
+        my_string = f.read()
+else:
+    print("You entered invalid input")                    # taking user input
+len_my_string = len(my_string)
+print ("Entered string is:",my_string)
+print("Your data is ",len_my_string * 7 , "bits long")
+
+letters = []
+only_letters = []
+for letter in my_string:
+    if letter not in letters:
+        frequency = my_string.count(letter)             #frequency of each letter repetition
+        letters.append(frequency)
+        letters.append(letter)
+        only_letters.append(letter)
+
+nodes = []
+while len(letters) > 0:
+    nodes.append(letters[0:2])
+    letters = letters[2:]                               # sorting according to frequency
+nodes.sort()
+huffman_tree = []
+huffman_tree.append(nodes)                             #Make each unique character as a leaf node
+
+def combine_nodes(nodes):
+    pos = 0
+    newnode = []
+    if len(nodes) > 1:
+        nodes.sort()
+        nodes[pos].append("1")                       # assigning values 1 and 0
+        nodes[pos+1].append("0")
+        combined_node1 = (nodes[pos] [0] + nodes[pos+1] [0])
+        combined_node2 = (nodes[pos] [1] + nodes[pos+1] [1])  # combining the nodes to generate pathways
+        newnode.append(combined_node1)
+        newnode.append(combined_node2)
+        newnodes=[]
+        newnodes.append(newnode)
+        newnodes = newnodes + nodes[2:]
+        nodes = newnodes
+        huffman_tree.append(nodes)
+        combine_nodes(nodes)
+    return huffman_tree                                     # huffman tree generation
+
+newnodes = combine_nodes(nodes)
+
+huffman_tree.sort(reverse = True)
+print("Huffman tree with merged pathways:")
+
+checklist = []
+for level in huffman_tree:
+    for node in level:
+        if node not in checklist:
+            checklist.append(node)
+        else:
+            level.remove(node)
+count = 0
+for level in huffman_tree:
+    print("Level", count,":",level)             #print huffman tree
+    count+=1
+print()
+
+letter_binary = []
+if len(only_letters) == 1:
+    lettercode = [only_letters[0], "0"]
+    letter_binary.append(lettercode*len(my_string))
+else:
+    for letter in only_letters:
+        code =""
+        for node in checklist:
+            if len (node)>2 and letter in node[1]:           #genrating binary code
+                code = code + node[2]
+        lettercode =[letter,code]
+        letter_binary.append(lettercode)
+print(letter_binary)
+print("Binary code generated:")
+for letter in letter_binary:
+    print(letter[0], letter[1])
+
+bitstring =""
+for character in my_string:
+    for item in letter_binary:
+        if character in item:
+            bitstring = bitstring + item[1]
+binary ="0b"+bitstring
+print("Your message as binary is:")
+print(bitstring)                                         # binary code generated
+
+uncompressed_file_size = len(my_string)*7
+compressed_file_size = len(binary)-2
+print("Your original file size was", uncompressed_file_size,"bits. The compressed size is:",compressed_file_size)
+print("This is a saving of ",uncompressed_file_size-compressed_file_size,"bits")
+output = open("compressed.txt","w+")
+print("Compressed file generated as compressed.txt")
+output = open("compressed.txt","w+")
+print("Decoding.......")
+output.write(bitstring)
+
+bitstring = str(binary[2:])
+uncompressed_string =""
+code =""
+for digit in bitstring:
+    code = code+digit
+    pos=0                                        #iterating and decoding
+    for letter in letter_binary:
+        if code ==letter[1]:
+            uncompressed_string=uncompressed_string+letter_binary[pos] [0]
+            code=""
+        pos+=1
+
+print("Your UNCOMPRESSED data is:")
+print(uncompressed_string)                         # uncompressed string
